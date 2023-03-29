2370cccc07bb2e2feda6ed6ae029e476bc0b23bc
Marion <mrobert148@gmail.com>
2023-03-29 12:48:40 +0200

    create form with wtform

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, PasswordField
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ["secret_key"]
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
 
 
 @app.route("/")
@@ -10,7 +19,8 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
