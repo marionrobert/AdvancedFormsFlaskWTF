f1ddfe01d4fb290da2cb342b5020bd955fa98e1a
Marion <mrobert148@gmail.com>
2023-03-29 13:10:24 +0200

    add validators for email input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 import os
 
 app = Flask(__name__)
@@ -8,8 +10,8 @@ app.secret_key = os.environ["secret_key"]
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email(), Length(min=8)])
+    password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label="Log in")
 
 
@@ -18,9 +20,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
