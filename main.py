ddf460ddeb341d3af2e5177fbe95684cb476f23f
Marion <mrobert148@gmail.com>
2023-03-29 12:54:06 +0200

    code improvements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm, Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 import os
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ app.secret_key = os.environ["secret_key"]
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label="Log in")
 
 
 @app.route("/")
